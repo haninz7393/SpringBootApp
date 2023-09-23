dd21940776114c4971babd05f8605026dfe15c56
haninz7393 <haninz7393@gmail.com>
2023-09-24 00:28:59 +0300

    adding departments

--- src/main/java/com/sol/SpringBootApp/controllers/DepartmentController.java
+++ src/main/java/com/sol/SpringBootApp/controllers/DepartmentController.java
@@ -1,5 +1,26 @@
 package com.sol.SpringBootApp.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.sol.SpringBootApp.services.DepartmentService;
+
+@Controller
+@RequestMapping("/departments")
 public class DepartmentController {
-    
+
+    private final DepartmentService departmentService;
+
+    public DepartmentController(DepartmentService departmentService) {
+        this.departmentService = departmentService;
+    }
+
+    @GetMapping
+    public String getAllDepartments(Model model) {
+        model.addAttribute("departments", departmentService.getAllDepartmentS());
+        return "departments";
+    }
+
 }
--- src/main/java/com/sol/SpringBootApp/controllers/EmployeeController.java
+++ src/main/java/com/sol/SpringBootApp/controllers/EmployeeController.java
@@ -1,23 +1,25 @@
 package com.sol.SpringBootApp.controllers;
 
-import java.util.ArrayList;
-import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.sol.SpringBootApp.models.Employees;
+import com.sol.SpringBootApp.services.EmployeeService;
 
 @Controller
 @RequestMapping("/employees")
 public class EmployeeController {
 
-    @GetMapping
-    public String getAllEmployees(Model model) {
-        model.addAttribute("employees", employees);
-        return "employees";
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
     }
 
+    @GetMapping
+    public String getAllEmployees(Model model) {
+        model.addAttribute("employees", employeeService.getAllEmployees());
+        return "employees";
+    }
 
 }
